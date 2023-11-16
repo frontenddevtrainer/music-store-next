3ff577bf24f5670e02c2bd7a7377187bab421a45
Admin <admin@example.com>
2023-11-16 06:21:07 +0000

    Added updates api startup and data binding.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import AlbumListing from "@/src/components/album-listing/album-listing";
 import Banner from "@/src/components/banner/banner";
 import Link from "next/link";
 
+const topAlbumAPI = process.env['TOP_ALBUM_SOURCE_API'] as string;
+const latestAlbumAPI = process.env['LATEST_ALBUM_SOURCE_API'] as string;
+
 export default function Home() {
   return (
     <main className="container mx-auto p-8">
       <Banner />
-      <AlbumListing heading="Top Albums"/>
-      <AlbumListing heading="Latest Albums"/>
+      <AlbumListing heading="Top Albums" source={topAlbumAPI}/>
+      <AlbumListing heading="Latest Albums" source={latestAlbumAPI}/>
     </main>
   );
 }
--- src/components/album-listing/album-listing.tsx
+++ src/components/album-listing/album-listing.tsx
@@ -1,39 +1,35 @@
+import { Album } from "@/src/interfaces/Album";
+import AlbumListingItem from "./album-listing-item";
+
 interface Props {
-    heading : string
+  heading: string;
+  source : string
 }
 
+async function getAlbumData(url: string) {
+  const response = await fetch(url);
 
-export default function AlbumListing({ heading } : Props) {
+  if (!response.ok) {
+    throw new Error("Unable to fetch data");
+  }
+
+  return response.json();
+}
+
+export default async function AlbumListing({ heading, source }: Props) {
+  const data = await getAlbumData(source);
+
+  console.log(data);
+
   return (
     <section className="mb-12">
       <h2 className="text-3xl text-green-400 mb-4">{heading}</h2>
       <div className="grid grid-cols-5 gap-4">
-        <div className="bg-gray-800 p-4 rounded relative">
-          <div className="relative">
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Album"
-              className="w-full rounded mb-2"
-            />
-            <button className="absolute bottom-2 right-2 bg-green-400 w-10 h-10 rounded-full flex items-center justify-center hover:bg-green-300">
-              <span className="material-icons text-white"> play_arrow </span>
-            </button>
-          </div>
-
-          <div className="text-left">
-            <h3 className="text-md font-bold mb-1">Album Name</h3>
-            <p className="text-sm mb-2 text-gray-400">By Singer</p>
-            <p className="text-sm mb-2 text-green-200">$9.99</p>
-
-            <a
-              href="#"
-              className="inline-flex items-center text-green-400 hover:text-green-300"
-            >
-              <span className="material-icons"> shopping_cart </span>
-              <span className="ml-1">Add to Cart</span>
-            </a>
-          </div>
-        </div>
+        {
+            data.map((album: Album)=>{
+                return <AlbumListingItem album={album}/>
+            })
+        }
       </div>
     </section>
   );
