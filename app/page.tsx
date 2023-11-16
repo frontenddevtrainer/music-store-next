6c15efb7cb871150f5e5c5bae1779043a0cba005
Admin <admin@example.com>
2023-11-16 05:54:39 +0000

    Added album listing and mock api setup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AlbumListing from "@/src/components/album-listing/album-listing";
 import Banner from "@/src/components/banner/banner";
 import Link from "next/link";
 
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <main className="container mx-auto p-8">
       <Banner />
+      <AlbumListing heading="Top Albums"/>
+      <AlbumListing heading="Latest Albums"/>
     </main>
   );
 }
