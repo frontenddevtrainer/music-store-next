c5fbbd2cf4be46568574551647bed01125e532e8
Admin <admin@example.com>
2023-11-17 06:42:27 +0000

    added people page.

---
+++ pages/people.tsx
@@ -0,0 +1,21 @@
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const people = await response.json();
+
+  return {
+    props: {
+      people,
+    },
+  };
+}
+
+export default function People({ people }: any) {
+
+  return (
+    <div>
+      {people && people.map((person: any) => {
+        return <p key={person.id}>{person.name}</p>;
+      })}
+    </div>
+  );
+}
